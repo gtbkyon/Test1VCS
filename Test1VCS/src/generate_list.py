8f5dcfb1a8ce0473a6bafe3646b51601541c60cc
Tanapon <whitelotus.kyo@gmail.com>
2017-02-03 02:05:46 +0000

    First commit

---
+++ Test1VCS/src/generate_list.py
@@ -0,0 +1,14 @@
+import random
+def generate_list():
+    alist = [x for x in range(random.randint(-10,10))]
+    return alist
+    
+    def printIt():
+        print(generate_list())
+    
+    def main():
+        printIt()
+        
+    if_name_=='_main_'
+        print("Test printIt():")
+        main()        

6cd0c208aff78e2712f24419444acc73cff65e71
Tanapon <whitelotus.kyo@gmail.com>
2017-02-03 02:26:09 +0000

    Error(s) corrected

--- Test1VCS/src/generate_list.py
+++ Test1VCS/src/generate_list.py
@@ -3,12 +3,12 @@ def generate_list():
     alist = [x for x in range(random.randint(-10,10))]
     return alist
     
-    def printIt():
-        print(generate_list())
+def printIt():
+    print(generate_list())
     
-    def main():
-        printIt()
+def main():
+    printIt()
         
-    if_name_=='_main_'
-        print("Test printIt():")
-        main()        
+if __name__=='__main__':
+    print"Test printIt():"
+    main()        
